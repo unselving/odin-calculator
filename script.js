93ccdae604266d5894ef31781e00dc6dfbf8e9d0
Michelle <michelle.lee0896@gmail.com>
2023-09-02 09:39:05 +0800

    Added event listeners to operator buttons - on click, operators get stored in operatorArray. Started to work on the operate() function.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let currentNumber=0;
-let newNumber=0;
+let currentNumber="";
+let currentOperator="";
+let numberArray=[];
+let operatorArray=[];
 let i=0;
 
 const numberOrder = [7,8,9,4,5,6,1,2,3,0];
@@ -7,8 +9,40 @@ const numbers = document.querySelectorAll('.number');
 numbers.forEach(function(currentBtn){
     currentBtn.value = numberOrder[i];
     currentBtn.addEventListener('click',()=>{
-        newNumber=currentBtn.value;
-        alert(newNumber);
+        currentNumber=currentNumber+currentBtn.value;
+        console.log(currentNumber);
     });
     i++;
 });
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(function(currentBtn){
+    currentBtn.addEventListener('click',()=>{
+        numberArray.push(currentNumber);
+        currentNumber="";
+        console.log(numberArray);
+        operatorArray.push(currentBtn.id);
+        console.log(operatorArray)
+    });
+});
+
+const equalsBtn = document.getElementById('equals');
+equalsBtn.addEventListener('click', operate());
+
+function operate(){
+    switch(operatorArray[0]){
+        case "add":
+            console.log("add");
+            break;
+        case "subtract":
+            console.log("subtract");
+            break;
+        case "multiply":
+            console.log("multiply");
+            break;
+        case "divide":
+            console.log("divide");
+            break;
+    }
+}
+
