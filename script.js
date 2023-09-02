940a0fa9eb49d5649c6b4a646e8802d279522b70
Michelle <michelle.lee0896@gmail.com>
2023-09-02 10:37:47 +0800

    Made everything look nice:

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-let currentNumber=0;
-let newNumber=0;
+let currentNumber="";
+let currentOperator="";
+let numberArray=[];
+let operatorArray=[];
 let i=0;
 
 const numberOrder = [7,8,9,4,5,6,1,2,3,0];
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(function(currentBtn){
     currentBtn.value = numberOrder[i];
+    currentBtn.addEventListener('mouseover',()=>{
+        currentBtn.style.backgroundColor='darkgrey';
+    });
+    currentBtn.addEventListener('mouseout',()=>{
+        currentBtn.style.backgroundColor='rgb(235,235,235)';
+    });
     currentBtn.addEventListener('click',()=>{
-        newNumber=currentBtn.value;
-        alert(newNumber);
+        currentNumber=currentNumber+currentBtn.value;
+        console.log(currentNumber);
     });
     i++;
 });
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(function(currentBtn){
+    currentBtn.addEventListener('mouseover',()=>{
+        currentBtn.style.backgroundColor='darkgrey';
+    });
+    currentBtn.addEventListener('mouseout',()=>{
+        currentBtn.style.backgroundColor='rgb(235,235,235)';
+    });
+    currentBtn.addEventListener('click',()=>{
+        numberArray.push(currentNumber);
+        currentNumber="";
+        console.log(numberArray);
+        operatorArray.push(currentBtn.id);
+        console.log(operatorArray)
+    });
+});
+
+const equalsBtn = document.getElementById('equals');
+equalsBtn.addEventListener('click', operate());
+
+function operate(){
+    switch(operatorArray[0]){
+        case "add":
+            console.log("add");
+            break;
+        case "subtract":
+            console.log("subtract");
+            break;
+        case "multiply":
+            console.log("multiply");
+            break;
+        case "divide":
+            console.log("divide");
+            break;
+    }
+}
+
